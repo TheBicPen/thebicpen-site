1d3d17537f1b1e5f81067c7cadc59fedc19e3f73
Alex <mashkal2000@gmail.com>
2025-02-23 00:00:30 -0800

    Initial site served by caddy
    Make a simple landing page and serve it over caddy.

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Installation script for the website. Installs all prerequisites on a Ubuntu
+# 24.04 machine
+
+set -x
+
+# Install build tools on the host - no point in putting this in a container
+sudo apt install -y just docker.io docker-compose git
